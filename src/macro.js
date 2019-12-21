8346b3860f900813be0753e58f355129e7596407
alexananev <48324900+alexananev@users.noreply.github.com>
2019-12-21 09:39:24 +0300

    Create macro.js

---
+++ src/macro.js
@@ -0,0 +1,4 @@
+var GOOGLE_FORM_LINK = "https://docs.google.com/forms/d/e/1FAIpQLScv1YTW7Du2jqd8KajnS5R9Lh7pV39x9sbCZuz4ovll5Iy4Yw/viewform"
+//форма для оплучения оценок
+var FORMSPREE_LINK = "https://formspree.io/xgeoaolz"
+//форма регистатрации на курс
