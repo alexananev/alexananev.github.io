0809ab717045434e82ed4e02b330bb506d83b3dd
alexananev <48324900+alexananev@users.noreply.github.com>
2019-12-21 10:06:47 +0300

    Update macro.js

--- src/macro.js
+++ src/macro.js
@@ -1,4 +1,6 @@
-var GOOGLE_FORM_LINK = "https://docs.google.com/forms/d/e/1FAIpQLScv1YTW7Du2jqd8KajnS5R9Lh7pV39x9sbCZuz4ovll5Iy4Yw/viewform"
 //форма для оплучения оценок
-var FORMSPREE_LINK = "https://formspree.io/xgeoaolz"
+var GOOGLE_FORM_LINK = "https://docs.google.com/forms/d/e/1FAIpQLScv1YTW7Du2jqd8KajnS5R9Lh7pV39x9sbCZuz4ovll5Iy4Yw/viewform"
 //форма регистатрации на курс
+var FORMSPREE_LINK = "https://formspree.io/xgeoaolz"
+//контактная информация для студентов
+var CONTACT_INFO = "Phne and Email"
